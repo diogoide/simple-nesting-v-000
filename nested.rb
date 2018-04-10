bedaef8294c3320f41c06eb3a3af97584aeedb7e
Diogo Ide <diogoide@gmail.com>
2018-04-10 00:21:40 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,9 +108,10 @@ def changing_alan
 		 alans_new_info = "GUI"
 		 programmer_hash [:alan_kay][:known_for] = alans_new_info
 		 programmer_hash [:alan_kay][:known_for]
-		 binding.pry
 
 end
+
+binding.pry
 
 def adding_to_dennis
 	programmer_hash =
